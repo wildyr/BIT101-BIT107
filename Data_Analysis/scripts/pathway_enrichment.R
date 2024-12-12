11e2c1aa6da2e03faca2066c2d0ef9f49c57f505
wildyr <wildyr@cardiff.ac.uk>
2024-12-12 00:43:45 +0000

    Removed KEGG and reactome analysis as these were incorrectly done. Refined the rest of the code

--- Data_Analysis/scripts/pathway_enrichment.R
+++ Data_Analysis/scripts/pathway_enrichment.R
@@ -22,22 +22,12 @@ library(gridExtra)
 
 # Read in filtered DEGs
 filtered <- read.csv("data/de_results_filtered.csv", row.names = 1)
-significant_genes <- rownames(filtered)
 upregulated <- filtered %>% filter(padj < 0.05 & log2FoldChange > 1)
 downregulated <- filtered %>% filter(padj < 0.05 & log2FoldChange < -1)
 up_significant_genes <- rownames(upregulated)
 down_significant_genes <- rownames(downregulated)
 
 # GO Enrichment
-go_results <- enrichGO(
-  gene          = significant_genes, 
-  OrgDb         = org.Hs.eg.db, 
-  keyType       = "SYMBOL",
-  ont           = "BP",
-  pAdjustMethod = "BH", 
-  pvalueCutoff  = 0.05,
-  qvalueCutoff  = 0.2
-)
 go_results_down <- enrichGO(
   gene          = down_significant_genes, 
   OrgDb         = org.Hs.eg.db, 
@@ -53,31 +43,29 @@ go_results_up <- enrichGO(
   keyType       = "SYMBOL",
   ont           = "BP",
   pAdjustMethod = "BH",
-  pvalueCutoff  = 0.05,
+  pvalueCutoff  = 1,
   qvalueCutoff  = 0.2
 )
 
 #write.csv(as.data.frame(go_results), "data/go_results.csv", row.names = FALSE)
 
 # Visualize GO Enrichment
-barplot(go_results,showCategory=10,title="Top 10 Enriched GO Terms (Biological Process)")
-barplot(go_results_up,showCategory=10,title="Top 10 Upregulated GO Terms (Biological Process)")
-barplot(go_results_down,showCategory=10,title="Top 10 Downregulated GO Terms (Biological Process)")
-head(go_results)
-head(go_results_up)
-head(go_results_down)
-dotplot(go_results, showCategory = 10)+ggtitle("Dotplot of GO Enrichment (Biological Process)")
+barplot(go_results_up,showCategory=10,title="Top 10 Up-regulated GO Terms")
+barplot(go_results_down,showCategory=10,title="Top 10 Down-regulated GO Terms")
+
+dotplot(go_results_up, showCategory = 10)+ggtitle("Dotplot of GO Enrichment")
+dotplot(go_results_down, showCategory = 10)+ggtitle("Dotplot of GO Enrichment")
 
 # Network plot for top 5 enriched terms
-cnetplot(go_results, showCategory = 5)+ggtitle("GO Enrichment Network Plot")
-#cnetplot(go_results_up, showCategory = 5)+ggtitle("GO Enrichment Network Plot")
-#cnetplot(go_results_down, showCategory = 5)+ggtitle("GO Enrichment Network Plot")
+cnetplot(go_results_up, showCategory = 5)+ggtitle("Up-regulated GO Enrichment Network Plot")+theme(plot.title = element_text(hjust = 0.5))
+
+cnetplot(go_results_down, showCategory = 5)+ggtitle("Down-regulated GO Enrichment Network Plot")+theme(plot.title = element_text(hjust = 0.5))
 
 # Combination plot
-go_bar <- barplot(go_results, showCategory = 10, title = "Top 10 Enriched GO Terms (Biological Process)")
-go_dot <- dotplot(go_results, showCategory = 10) + ggtitle("Dotplot of GO Enrichment (Biological Process)")
-go_network <- cnetplot(go_results, showCategory = 5) + 
-  ggtitle("GO Enrichment Network Plot") +
+go_bar <- barplot(go_results_down, showCategory = 10, title = "Top 10 Down-regulated GO Terms ")
+go_dot <- dotplot(go_results_down, showCategory = 10) + ggtitle("Dotplot of Down-regulated GO Enrichment")
+go_network <- cnetplot(go_results_down, showCategory = 5) + 
+  ggtitle("Down-regulated GO Enrichment Network Plot") +
   theme(plot.title = element_text(hjust = 0.5))
 
 combined_plot_go <- grid.arrange(
@@ -87,44 +75,3 @@ combined_plot_go <- grid.arrange(
   heights = c(1, 2),
   layout_matrix = matrix(c(1, 2, 3, 3), nrow = 2, byrow = TRUE)
 )
-
-# KEGG Pathway Enrichment Analysis
-# Convert SYMBOL to ENTREZID
-gene_ids <- bitr(significant_genes, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Hs.eg.db)
-
-# KEGG enrichment
-kegg_results <- enrichKEGG(
-  gene         = gene_ids$ENTREZID, 
-  organism     = 'hsa',
-  pAdjustMethod = "BH",
-  pvalueCutoff  = 0.05
-)
-#write.csv(as.data.frame(kegg_results), "data/kegg_results.csv", row.names = FALSE)
-
-# Visualize KEGG Enrichment
-barplot(kegg_results, showCategory = 10, title = "Top 10 Enriched KEGG Pathways")
-dotplot(kegg_results, showCategory = 10)+ggtitle("Dotplot of KEGG Enrichment")
-
-kegg_bar <- barplot(kegg_results, showCategory = 10, title = "Top 10 Enriched KEGG Pathways")
-kegg_dot <- dotplot(kegg_results, showCategory = 10)+ggtitle("Dotplot of KEGG Enrichment")
-
-combined_plot_kegg <- grid.arrange(kegg_bar, kegg_dot, ncol = 2, nrow = 1)
-
-# Reactome Pathway Analysis
-reactome_results <- enrichPathway(
-  gene = gene_ids$ENTREZID, 
-  organism = "human", 
-  pvalueCutoff = 0.05
-)
-#write.csv(as.data.frame(reactome_results), "data/reactome_results.csv", row.names = FALSE)
-
-# Visualize Reactome Pathway Enrichment
-barplot(reactome_results, showCategory = 10, title = "Top 10 Enriched Reactome Pathways")
-dotplot(reactome_results, showCategory = 10)+ggtitle("Dotplot of Reactome Enrichment")
-
-react_bar <- barplot(reactome_results, showCategory = 10, title = "Top 10 Enriched Reactome Pathways")
-react_dot <- dotplot(reactome_results, showCategory = 10)+ggtitle("Dotplot of Reactome Enrichment")
-
-combined_plot_kegg <- grid.arrange(react_bar, react_dot, ncol = 2, nrow = 1)
-
-sessionInfo()
